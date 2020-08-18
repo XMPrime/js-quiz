013e392bc23a58918379de7e94ff4d72bd6af17c
Jason Chen <jason.l.chen@gmail.com>
2020-08-18 11:56:30 -0700

    Revert "Minor clean up"
    This reverts commit 3afb6dc059eaacd38a15bdce2638b63e3471f64c.

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AnswerObject } from "../App";
 import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 
