fe6382d2ffad9b308548b98146608b28a775d06b
Jason Chen <jason.l.chen@gmail.com>
2020-08-18 14:19:54 -0700

    Update quiz-scraper.js

--- functions/quiz-scraper.js
+++ functions/quiz-scraper.js
@@ -10,12 +10,76 @@ exports.handler = async (event) => {
   });
   const page = await browser.newPage();
   await page.goto("https://github.com/lydiahallie/javascript-questions/");
+
   const questions = await page.$$eval("h6", (questions) =>
     questions.map((question) => question.textContent)
   );
+
+  const codeBlocks = await page.$$eval("h6", (blocks) =>
+    blocks.map((block) => {
+      if (block.nextElementSibling.className.includes("highlight"))
+        return block.nextElementSibling.children[0].textContent;
+      else return "";
+    })
+  );
+
+  const choices = await page.$$eval("ul > li", (choices) =>
+    choices
+      .map((choice) => {
+        if (choice.textContent[1] === ":") return choice.textContent;
+        return "";
+      })
+      .filter((choice) => choice !== "")
+  );
+
+  const choiceSets = createChoiceSets(choices);
+
+  const answers = await page.$$eval("details > h4", (answers) =>
+    answers.map((answer) => answer.textContent[answer.textContent.length - 1])
+  );
+
+  const answerDetails = await page.$$eval("details > p, h6", (details) =>
+    details
+      .map((detail) => detail.textContent)
+      .filter((detail) => detail.length >= 3)
+  );
+
+  const answerDetailSets = createAnswerDetailSets(answerDetails);
   await browser.close();
+
   return {
     statusCode: 200,
-    body: `${questions[0]}!`,
+    body: { questions, codeBlocks, choiceSets, answers, answerDetailSets },
   };
+};
+
+const createChoiceSets = (array) => {
+  let choiceSets = [];
+  let set = [array[0]];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].charCodeAt(0) - array[i - 1].charCodeAt(0) === 1) {
+      set.push(array[i]);
+    } else {
+      choiceSets.push(set);
+      set = [array[i]];
+    }
+    if (i === array.length - 1) choiceSets.push(set);
+  }
+  return choiceSets;
+};
+
+const createAnswerDetailSets = (array) => {
+  const regex = /^[0-9]{1,3}\.\s/;
+  let answerSets = [];
+  let set = [];
+  for (let i = 1; i < array.length; i++) {
+    if (!regex.test(array[i])) {
+      set.push(array[i]);
+    }
+    if (regex.test(array[i]) || i === array.length - 1) {
+      answerSets.push(set);
+      set = [];
+    }
+  }
+  return answerSets;
 };
