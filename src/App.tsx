4c6564a9b943b1f0011648bf098b144f08832d59
Jason Chen <jason.l.chen@gmail.com>
2020-08-19 20:21:41 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -145,27 +145,27 @@ function App() {
     setShowAnswer(false);
   };
 
-  const codeBlock = `const randomValue = 21;
+  // const codeBlock = `const randomValue = 21;
 
-  function getInfo() {
-    console.log(typeof randomValue);
-    const randomValue = 'Lydia Hallie';
-  }
-  
-  getInfo();`;
+  // function getInfo() {
+  //   console.log(typeof randomValue);
+  //   const randomValue = 'Lydia Hallie';
+  // }
 
-  const question = "129. What's the output?";
-  const choices = [
-    "A: asl;dgfjkalsd",
-    "B: asl;dgfjkalsd",
-    "C: asl;dgfjkalsd",
-    "D: asl;dgfjkalsd",
-  ];
-  const answer = "A";
-  const answerDetails = [
-    "Answer: D",
-    "Variables declared with the const keyword are not referencable before their initialization: this is called the temporal dead zone. In the getInfo function, the variable randomValue is scoped in the functional scope of getInfo. On the line where we want to log the value of typeof randomValue, the variable randomValue isn't initialized yet: a ReferenceError gets thrown! The engine didn't go down the scope chain since we declared the variable randomValue in the getInfo function.",
-  ];
+  // getInfo();`;
+
+  // const question = "129. What's the output?";
+  // const choices = [
+  //   "A: asl;dgfjkalsd",
+  //   "B: asl;dgfjkalsd",
+  //   "C: asl;dgfjkalsd",
+  //   "D: asl;dgfjkalsd",
+  // ];
+  // const answer = "A";
+  // const answerDetails = [
+  //   "Answer: D",
+  //   "Variables declared with the const keyword are not referencable before their initialization: this is called the temporal dead zone. In the getInfo function, the variable randomValue is scoped in the functional scope of getInfo. On the line where we want to log the value of typeof randomValue, the variable randomValue isn't initialized yet: a ReferenceError gets thrown! The engine didn't go down the scope chain since we declared the variable randomValue in the getInfo function.",
+  // ];
 
   return (
     <>
