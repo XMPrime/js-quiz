d16e6d76a3faa8f4e22c11be2c7198163a06cfeb
Jason Chen <jason.l.chen@gmail.com>
2020-08-24 14:52:06 -0700

    Putting quiz-scraper back to .js

--- functions/quiz-scraper.js
+++ functions/quiz-scraper.js
@@ -1,18 +1,6 @@
-// import chromium from "chrome-aws-lambda";
-import {
-  APIGatewayProxyEvent,
-  Context,
-  APIGatewayProxyResult,
-} from "aws-lambda";
-// type APIGatewayProxyResult = AWSLambda.APIGatewayProxyResult;
 const chromium = require("chrome-aws-lambda");
 
-type textContent = { textContent: string }[];
-type nextElementSibling = {
-  nextElementSibling: { className: string; children: textContent };
-}[];
-
-const createChoiceSets = (array: string[]): string[][] => {
+const createChoiceSets = (array) => {
   let choiceSets = [];
   let set = [array[0]];
   for (let i = 1; i < array.length; i++) {
@@ -28,7 +16,7 @@ const createChoiceSets = (array: string[]): string[][] => {
   return choiceSets;
 };
 
-const createAnswerDetailSets = (array: string[]): string[][] => {
+const createAnswerDetailSets = (array) => {
   const regex = /^[0-9]{1,3}\.\s/;
   let answerSets = [];
   let set = [];
@@ -43,9 +31,8 @@ const createAnswerDetailSets = (array: string[]): string[][] => {
   }
   return answerSets;
 };
-// export const handler = async (): Promise<APIGatewayProxyResult> => {
-exports.handler = async (): Promise<APIGatewayProxyResult> => {
-  
+export const handler = async () => {
+  // exports.handler = async () => {
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -55,11 +42,11 @@ exports.handler = async (): Promise<APIGatewayProxyResult> => {
   const page = await browser.newPage();
   await page.goto("https://github.com/lydiahallie/javascript-questions/");
 
-  const questions = await page.$$eval("h6", (questions: textContent) =>
+  const questions = await page.$$eval("h6", (questions) =>
     questions.map((question) => question.textContent)
   );
 
-  const codeBlocks = await page.$$eval("h6", (blocks: nextElementSibling) =>
+  const codeBlocks = await page.$$eval("h6", (blocks) =>
     blocks.map((block) => {
       if (block.nextElementSibling.className.includes("highlight"))
         return block.nextElementSibling.children[0].textContent;
@@ -67,7 +54,7 @@ exports.handler = async (): Promise<APIGatewayProxyResult> => {
     })
   );
 
-  const choices = await page.$$eval("ul > li", (choices: textContent) =>
+  const choices = await page.$$eval("ul > li", (choices) =>
     choices
       .map((choice) => {
         if (choice.textContent[1] === ":") return choice.textContent;
@@ -78,16 +65,14 @@ exports.handler = async (): Promise<APIGatewayProxyResult> => {
 
   const choiceSets = createChoiceSets(choices);
 
-  const answers = await page.$$eval("details > h4", (answers: textContent) =>
+  const answers = await page.$$eval("details > h4", (answers) =>
     answers.map((answer) => answer.textContent[answer.textContent.length - 1])
   );
 
-  const answerDetails = await page.$$eval(
-    "details > p, h6",
-    (details: textContent) =>
-      details
-        .map((detail) => detail.textContent)
-        .filter((detail) => detail.length >= 3)
+  const answerDetails = await page.$$eval("details > p, h6", (details) =>
+    details
+      .map((detail) => detail.textContent)
+      .filter((detail) => detail.length >= 3)
   );
 
   const answerDetailSets = createAnswerDetailSets(answerDetails);
